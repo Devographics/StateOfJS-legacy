2e3826ee8c55cee6784c4b72dbdf981c7a916ed0
Sacha Greif <sacha357@gmail.com>
2016-08-23 08:39:21 +0900

    cleaning up

--- pages/_newsletter.js
+++ pages/_newsletter.js
@@ -6,7 +6,7 @@ export default class Newsletter extends React.Component {
     return (
       <div className="newsletter">
         <div id="mc_embed_signup">
-          <form action="//sachagreif.us2.list-manage.com/subscribe/post?u=b5af47765edbd2fc173dbf27a&amp;id=d8282e7e96" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
+          <form action="//sachagreif.us2.list-manage.com/subscribe/post?u=b5af47765edbd2fc173dbf27a&amp;id=d8282e7e96" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
             <input type="email" placeholder="Your email" name="EMAIL" className="required email" id="mce-EMAIL"/>
             <input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" className="button"/>
           </form>
