cb40c0c60ba9b4e0d20ddbc083ec74de1f05cb00
Michael Shilman <shilman@lab80.co>
2016-09-01 09:43:14 -0700

    Custom bar chart labels

--- pages/results.js
+++ pages/results.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { includes, values } from 'lodash'
+import { includes, values, sumBy, isString } from 'lodash'
 import DocumentTitle from 'react-document-title'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'Recharts'
 import data from '../data/frontend.json'
@@ -9,6 +9,21 @@ const FILTER = {
   ALL: 'All',
   INTEREST: 'Interest',
   SATISFACTION: 'Satisfaction',
+}
+
+const CustomLabel = (args) => {
+  console.log('args', args)
+  const { key, index, value, x, y } = args
+  const total = sumBy(values(value.data), (d) => (isString(d) ? 0 : d))
+  const percent = (100 * (value[1] - value[0]) / total).toFixed(0)
+  const label = `${percent}%`
+  return (
+    <g key={key} className="recharts-cartesian-axis-label">
+      <text className="label" x={x} y={y + 10 + args.height/2} textAnchor="middle" fontSize="12" fill="white" >
+        {label}
+      </text>
+    </g>
+  )
 }
 
 class ResultsChart extends React.Component {
@@ -25,15 +40,15 @@ class ResultsChart extends React.Component {
   render() {
     const fills = this.getFills()
     return (
-      <BarChart width={600} height={400} data={data} barCategoryGap="30%" margin={{top: 75, right: 30, left: 20, bottom: 5}}>
-        <XAxis dataKey="Framework" tickLine={false} />
+      <BarChart width={600} height={400} data={data} barCategoryGap="30%" margin={{top: 60, right: 20, left: 20, bottom: 5}}>
+        <XAxis dataKey="Framework" tickLine={false} axisLine={{ stroke: '#666' }} />
         <Legend align="left" wrapperStyle={{top: 20}}/>
         <Tooltip/>
-        <Bar className="use-again" isAnimationActive={false} dataKey="I've used it before, and would use it again" stackId="a" fill={fills.useAgain} />
-        <Bar className="not-use-again" isAnimationActive={false} dataKey="I've used it before, and would not use it again" stackId="a" fill={fills.notAgain} />
-        <Bar className="want-to-learn" isAnimationActive={false} dataKey="I've heard of it, and would like to learn it" stackId="a" fill={fills.wantToLearn} />
-        <Bar className="not-interested" isAnimationActive={false} dataKey="I've heard of it, and am not interested" stackId="a" fill={fills.notInterested} />
-        <Bar className="never-heard" isAnimationActive={false} dataKey="I've never heard of it" stackId="a" fill={fills.neverHeard} />
+        <Bar className="use-again" isAnimationActive={false} dataKey="I've used it before, and would use it again" stackId="a" fill={fills.useAgain} label={CustomLabel} />
+        <Bar className="not-use-again" isAnimationActive={false} dataKey="I've used it before, and would not use it again" stackId="a" fill={fills.notAgain} label={CustomLabel} />
+        <Bar className="want-to-learn" isAnimationActive={false} dataKey="I've heard of it, and would like to learn it" stackId="a" fill={fills.wantToLearn} label={CustomLabel} />
+        <Bar className="not-interested" isAnimationActive={false} dataKey="I've heard of it, and am not interested" stackId="a" fill={fills.notInterested} label={CustomLabel} />
+        <Bar className="never-heard" isAnimationActive={false} dataKey="I've never heard of it" stackId="a" fill={fills.neverHeard} label={CustomLabel} />
       </BarChart>
     )
   }
@@ -42,6 +57,24 @@ class ResultsChart extends React.Component {
 ResultsChart.propTypes = {
   filter: React.PropTypes.string,
 }
+
+const ResultsFilter = ({ handleSelect }) => (
+  <div className="filter">
+    <h3>Filter</h3>
+    <ul>
+      {values(FILTER).map((filter) => {
+        return (
+          <li key={filter} className={filter.toLowerCase()} onClick={(e) => {
+            e.preventDefault()
+            handleSelect(filter)
+          }}>
+            {filter}
+          </li>
+        )
+      })}
+    </ul>
+  </div>
+)
 
 export default class Results extends React.Component {
   constructor() {
@@ -54,21 +87,11 @@ export default class Results extends React.Component {
     const filter = this.state
     return (
       <DocumentTitle title="Results">
-        <div>
+        <div className={this.state.filter.toLowerCase()}>
           <h2>Results</h2>
           <img width="100%" src="/images/chart.png" />
-          <ol>
-            {values(FILTER).map((filter) => {
-              return (
-                <li key={filter} onClick={(e) => {
-                  e.preventDefault()
-                  this.setState({ filter })
-                }}>
-                  {filter}
-                </li>
-              )
-            })}
-          </ol>
+          <img width="100%" src="/images/chart2.png" />
+          <ResultsFilter handleSelect={(filter) => this.setState({ filter })} />
           <ResultsChart filter={this.state.filter} />
         </div>
       </DocumentTitle>
