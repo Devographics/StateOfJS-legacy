206a1a169053a965aeb2f354f580bca76fb01f8a
Michael Shilman <shilman@lab80.co>
2016-09-02 18:01:18 -0700

    Remove tooltip, click to toggle

--- pages/results.js
+++ pages/results.js
@@ -44,11 +44,11 @@ const SECTIONS = {
   }
 }
 
-const Label = ({ currentFilter, key, index, value, x, y, height }) => {
-  
+const Label = ({ showPercent, currentFilter, key, index, value, x, y, height }) => {
+
   // overall total for all values (not used)
   // let total = sumBy(values(value.data), (d) => (isString(d) ? 0 : d))
-  
+
   // get all currently highlighted sections
   const highlightedSections = filter(SECTIONS, section => {
     return includes(section.filters, currentFilter)
@@ -60,7 +60,11 @@ const Label = ({ currentFilter, key, index, value, x, y, height }) => {
     subtotal += value.data[section.string]
   })
 
-  const label = `${(100 * (value[1] - value[0]) / subtotal).toFixed(0)}%`
+  const count = (value[1] - value[0])
+  const label = showPercent
+    ? `${(100 * count / subtotal).toFixed(0)}%`
+    : count
+
   return (
     <g key={key} className="recharts-cartesian-axis-label">
       <text className="label" x={x} y={y + 10 + height/2} textAnchor="middle" fontSize="12" fill="white" >
@@ -77,14 +81,15 @@ class ResultsChart extends React.Component {
     const section = SECTIONS[sectionName]
     return includes(section.filters, this.props.filter) ? section.onColor : section.offColor
   }
-  
+
   render() {
-    const CustomLabel = <Label currentFilter={this.props.filter}/>
+    const { filter, showPercent, handleToggle } = this.props
+    const CustomLabel = <Label currentFilter={filter} showPercent={showPercent} />
     return (
-      <BarChart width={600} height={400} data={data} barCategoryGap="30%" margin={{top: 60, right: 20, left: 20, bottom: 5}}>
+      <BarChart width={600} height={400} data={data} barCategoryGap="30%" margin={{top: 60, right: 20, left: 20, bottom: 5}} onClick={handleToggle} >
         <XAxis dataKey="Framework" tickLine={false} axisLine={{ stroke: '#666' }} />
         <Legend align="left" wrapperStyle={{top: 20}}/>
-        <Tooltip/>
+        {/* <Tooltip/> */}
         <Bar className="use-again" isAnimationActive={false} dataKey="I've used it before, and would use it again" stackId="a" fill={this.getFill("useAgain")} label={CustomLabel} />
         <Bar className="not-use-again" isAnimationActive={false} dataKey="I've used it before, and would not use it again" stackId="a" fill={this.getFill("notAgain")} label={CustomLabel} />
         <Bar className="want-to-learn" isAnimationActive={false} dataKey="I've heard of it, and would like to learn it" stackId="a" fill={this.getFill("wantToLearn")} label={CustomLabel} />
@@ -122,8 +127,10 @@ export default class Results extends React.Component {
   constructor() {
     super()
     this.handleSelect = this.handleSelect.bind(this)
+    this.handleToggle = this.handleToggle.bind(this)
     this.state = {
       filter: FILTER.ALL,
+      showPercent: true,
     }
   }
 
@@ -131,14 +138,20 @@ export default class Results extends React.Component {
     this.setState({ filter })
   }
 
+  handleToggle() {
+    const showPercent = !this.state.showPercent
+    console.log('pct', showPercent);
+    this.setState({ showPercent })
+  }
+
   render () {
-    const filter = this.state
+    const { filter, showPercent } = this.state
     return (
       <DocumentTitle title="Results">
         <div className={this.state.filter.toLowerCase()}>
           <h2>Results</h2>
           <ResultsFilter handleSelect={this.handleSelect} />
-          <ResultsChart filter={this.state.filter} />
+          <ResultsChart filter={filter} showPercent={showPercent} handleToggle={this.handleToggle}/>
         </div>
       </DocumentTitle>
     )
