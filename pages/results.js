ef34e74ef3c91b4cf5a125a2fdd3fb870b078234
Michael Shilman <shilman@lab80.co>
2016-09-04 01:54:43 -0700

    sections => choices, custom scroll handler

--- pages/results.js
+++ pages/results.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import DocumentTitle from 'react-document-title'
 import { StickyContainer, Sticky } from 'react-sticky'
-import WayPoint from 'react-waypoint'
-import { throttle } from 'lodash'
+import { throttle, sortBy } from 'lodash'
 
 import flavors from '../data/flavors.json'
 import frontend from '../data/frontend.json'
 import StackedBar from './charts/_stackedbar.js'
 import './_results.scss'
+
+const SECTIONS = {
+  FLAVORS: 'Flavors',
+  FRAMEWORKS: 'Frameworks',
+}
 
 const FILTERS = {
   ALL: 'All',
@@ -15,7 +19,7 @@ const FILTERS = {
   SATISFACTION: 'Satisfaction',
 }
 
-const SECTIONS = {
+const CHOICES = {
   neverHeard: {
     string: "I've never heard of it",
     onColor: '#e8e8e8',
@@ -48,42 +52,90 @@ const SECTIONS = {
   }
 }
 
+class FilterPoint extends React.Component {
+  componentDidMount() {
+    this.props.breakpoints.push({
+      filter: this.props.filter,
+      top: this._ref.getBoundingClientRect().top
+    })
+  }
+  render() {
+    return <span ref={(e) => this._ref = e} style={{fontSize: 0}} />;
+  }
+}
+
+FilterPoint.getWindow = () => {
+  if (typeof window !== 'undefined') {
+    return window;
+  }
+}
+
 class Results extends React.Component {
   constructor(props) {
     super(props)
+    this._offset = 0
     this.filterUpdate = this.filterUpdate.bind(this)
-    this.filterPoint = this.filterPoint.bind(this)
-    this._filterNameToBreakpoints = []
+    this._handleScroll = throttle(this._handleScroll.bind(this), 100)
+    this._handleResize = throttle(this._handleResize.bind(this), 100)
+    this._sectionToBreakpoints = {
+      [SECTIONS.FLAVORS]: [],
+      [SECTIONS.FRAMEWORKS]: [],
+    }
     this.state = {
-      filterFlavors: FILTERS.ALL,
-      filterFrameworks: FILTERS.ALL,
+      [SECTIONS.FLAVORS]: FILTERS.ALL,
+      [SECTIONS.FRAMEWORKS]: FILTERS.ALL,
     }
   }
 
-  filterUpdate(filterName, value) {
-    console.log('updating', filterName, value);
-    this.setState({ [filterName]: value })
+  componentDidMount() {
+    if (!FilterPoint.getWindow()) return
+    window.addEventListener('resize', this._handleResize)
+    window.addEventListener('scroll', this._handleScroll)
+
+    Object.keys(this._sectionToBreakpoints).forEach((section) => {
+      this._sectionToBreakpoints[section] = sortBy(this._sectionToBreakpoints[section], (b) => -b.top)
+    })
   }
 
-  filterPoint(filterName, enterValue, leaveValue, text) {
+  componentWillUnmount() {
+    if (!FilterPoint.getWindow()) return
+    window.addEventListener('resize', this._handleResize)
+    window.addEventListener('scroll', this._handleScroll)
+  }
+
+  filterUpdate(section, value) {
+    this.setState({ [section]: value })
+  }
+
+  _activeFilter(breakpoints, top) {
+    if(!breakpoints.length) return null
+    const active = breakpoints.find((b) => b.top < top)
+    return (active && active.filter) || breakpoints[0].filter
+  }
+
+  _handleScroll() {
+    const doc = document
+    const body = doc.body
+    const scrollTop = (doc.documentElement && doc.documentElement.scrollTop) || body.scrollTop
+    const scrollHeight = window.innerHeight // (doc.documentElement && doc.documentElement.scrollHeight) || body.scrollHeight
+
+    const updatedState = {}
+    const top = scrollTop + scrollHeight / 2
+    Object.keys(this._sectionToBreakpoints).forEach((section) => {
+      const filter = this._activeFilter(this._sectionToBreakpoints[section], top)
+      if(filter) updatedState[section] = filter
+    })
+    this.setState(updatedState)
+  }
+
+  _handleResize() {
+    this._offset = window.innerHeight / 2
+    this._handleScroll()
+  }
+
+  filterPoint(section, filter) {
     return (
-      <WayPoint
-        throttleHandler={(scrollHandler) => throttle(scrollHandler, 50)}
-        onEnter={() => {
-          if(enterValue) {
-            console.log('enter', enterValue)
-            this.filterUpdate(filterName, enterValue)
-          }
-        }}
-        onLeave={() => {
-          if(leaveValue) {
-            console.log('leave', leaveValue)
-            this.filterUpdate(filterName, leaveValue)
-          }
-        }}
-      >
-        {text}
-      </WayPoint>
+      <FilterPoint breakpoints={this._sectionToBreakpoints[section]} filter={filter} />
     )
   }
 
@@ -94,21 +146,21 @@ class Results extends React.Component {
           <div className="section">
             <StickyContainer className="sticky-container">
               <Sticky className="sticky">
-                <StackedBar identifier="Flavor" title="JavaScript Flavors" data={flavors} sections={SECTIONS} filters={FILTERS} filter={this.state.filterFlavors} handleSelect={(filter) => this.filterUpdate('filterFlavors', filter)} />
+                <StackedBar identifier="Flavor" title="JavaScript Flavors" data={flavors} choices={CHOICES} filters={FILTERS} filter={this.state[SECTIONS.FLAVORS]} handleSelect={(filter) => this.filterUpdate(SECTIONS.FLAVORS, filter)} />
               </Sticky>
             </StickyContainer>
 
             <div className="section-contents">
               <h1>JavaScript Flavors</h1>
-              <p>{this.filterPoint('filterFlavors', FILTERS.ALL, FILTERS.INTEREST)} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <p>{this.filterPoint('filterFlavors', FILTERS.INTEREST, FILTERS.SATISFACTION)} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <p>{this.filterPoint('filterFlavors', FILTERS.SATISFACTION, FILTERS.SATISFACTION)} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              <p>{this.filterPoint(SECTIONS.FLAVORS, FILTERS.ALL)} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              <p>{this.filterPoint(SECTIONS.FLAVORS, FILTERS.INTEREST)} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              <p>{this.filterPoint(SECTIONS.FLAVORS, FILTERS.SATISFACTION)} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
           </div>
           <div className="section">
             <StickyContainer className="sticky-container">
               <Sticky className="sticky">
-                <StackedBar identifier="Framework" title="Front-end Frameworks" data={frontend} sections={SECTIONS} filters={FILTERS} filter={this.state.filterFrameworks} handleSelect={(filter) => this.filterUpdate('filterFrameworks', filter)}  />
+                <StackedBar identifier="Framework" title="Front-end Frameworks" data={frontend} choices={CHOICES} filters={FILTERS} filter={this.state[SECTIONS.FRAMEWORKS]} handleSelect={(filter) => this.filterUpdate(SECTIONS.FRAMEWORKS, filter)}  />
               </Sticky>
             </StickyContainer>
 
