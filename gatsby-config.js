1d071604076f32a8bda6f1e10aa79506e7b5ffbb
SachaG <info@sachagreif.com>
2017-07-10 09:08:15 +0900

    add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`,
+        path: `${__dirname}/data/`,
       },
     },
     {
