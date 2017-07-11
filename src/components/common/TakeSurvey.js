8761bda98ce1d7b5dca55a690bb9eef9ce014059
SachaG <info@sachagreif.com>
2017-07-11 10:41:48 +0900

    fix browser data

--- src/components/common/TakeSurvey.js
+++ src/components/common/TakeSurvey.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import bowser from 'bowser'
 import axios from 'axios'
+import ReactGA from 'react-ga'
 
 const getOS = () => {
   const detectedOS = []
@@ -44,6 +45,8 @@ export default class TakeSurvey extends React.Component {
     // script.src = "//platform.twitter.com/widgets.js";
     // script.async = true;
     // document.body.appendChild(script);
+
+    // geo data
     axios.get('http://freegeoip.net/json/').then(response => {
       // console.log(response)
       this.setState({
@@ -54,15 +57,23 @@ export default class TakeSurvey extends React.Component {
       console.log(error)
     })
 
-    const browserInfo = {
+    // ga id
+    if (ReactGA.ga() && ReactGA.ga().getAll) {
+      this.setState({
+        gaId: ReactGA.ga().getAll()[0].get('clientId'),
+      })
+    }
+
+    // browser data
+    const browserData = {
       device: getDevice(),
       browser: bowser.name,
       version: bowser.version,
       os: getOS(),
       referrer: document.referrer,
-      gaId: ga.getAll()[0].get('clientId')
     }
-    this.setState(browserInfo)
+    this.setState(browserData)
+
   }
 
   render () {
@@ -70,7 +81,7 @@ export default class TakeSurvey extends React.Component {
       <div className="take-survey">
           <a className="button large-button" href={`https://stateofjs.typeform.com/to/S5iLk9?gaid=${this.state.gaId}&referrer=${this.state.referrer}&city=${this.state.city}&location=${this.state.location}&device=${this.state.device}&browser=${this.state.browser}&version=${this.state.version}&os=${this.state.os}`}>Take the Survey</a>
           
-          <p className="take-survey-note">Note: to improve the relevancy of the results, we keep track of anonymous information such as your referrer, location, device, browser, and OS.</p>
+          <p className="take-survey-note">Note: to improve results relevance, we keep track of anonymous information such as your referrer, location, device, browser, and OS.</p>
       </div>
     )
   }
