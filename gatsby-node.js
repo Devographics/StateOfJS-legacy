5c3373bcf9e0a3104b122b74a0a291ad7f3b9f9a
SachaG <358832+SachaG@users.noreply.github.com>
2017-11-09 17:24:28 +0900

    fixing dependencies and build process

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,54 +29,54 @@ exports.modifyWebpackConfig = function ({config, stage}) {
   return config
 }
 
-exports.onPostBuild = function (args, pluginOptions) {
+// exports.onPostBuild = function (args, pluginOptions) {
 
-  // perform actions on pages here
-  const svgpath = 'public/svgcontainer/index.html'
+//   // perform actions on pages here
+//   const svgpath = 'public/svgcontainer/index.html'
 
-  fs.readFile(svgpath, 'utf8', (err1, data) => {
-    if (err1) throw err1
+//   fs.readFile(svgpath, 'utf8', (err1, data) => {
+//     if (err1) throw err1
 
-    // parse SVGContainer file
-    const $ = cheerio.load(data)
-    const svgs = $('.svg-block')
+//     // parse SVGContainer file
+//     const $ = cheerio.load(data)
+//     const svgs = $('.svg-block')
 
-    // iterate over SVG blocks
-    svgs.each((i, svg) => {
-      const svgId = $(svg).attr('id')
-      // insert background color rect
-      $(svg).find('.recharts-surface').prepend('<rect x="0" y="0" width="100%" height="100%" fill="#fffef0" />')
-      const svgContents = $(svg).find('.recharts-wrapper').html()
-      const svgPath = `public/exports/svg/${svgId}.svg`
-      const pngPath = `public/exports/png/${svgId}.png`
+//     // iterate over SVG blocks
+//     svgs.each((i, svg) => {
+//       const svgId = $(svg).attr('id')
+//       // insert background color rect
+//       $(svg).find('.recharts-surface').prepend('<rect x="0" y="0" width="100%" height="100%" fill="#fffef0" />')
+//       const svgContents = $(svg).find('.recharts-wrapper').html()
+//       const svgPath = `public/exports/svg/${svgId}.svg`
+//       const pngPath = `public/exports/png/${svgId}.png`
 
-      console.log(`// exporting ${svgId}…`)
+//       console.log(`// exporting ${svgId}…`)
 
-      // export SVG
+//       // export SVG
 
-      fs.exists(svgPath, exists => {
-        const options = exists ? {} : { flag: 'wx' }
-        fs.writeFile(svgPath, svgContents, options, (err, data) => {
-          if (err) console.log(err)
-        })
-      })
+//       fs.exists(svgPath, exists => {
+//         const options = exists ? {} : { flag: 'wx' }
+//         fs.writeFile(svgPath, svgContents, options, (err, data) => {
+//           if (err) console.log(err)
+//         })
+//       })
 
-      // export PNG
+//       // export PNG
 
-      // see https://cloudconvert.com/api/convert/svg-to-png
-      try {
-        cloudconvert.convert({
-          inputformat: 'svg',
-          outputformat: 'png',
-          input: 'raw',
-          filename: `${svgId}.svg`,
-          file: svgContents,
-        }).pipe(fs.createWriteStream(pngPath))
-      } catch (err) { console.log(err) }
-    })
-  })
+//       // see https://cloudconvert.com/api/convert/svg-to-png
+//       try {
+//         cloudconvert.convert({
+//           inputformat: 'svg',
+//           outputformat: 'png',
+//           input: 'raw',
+//           filename: `${svgId}.svg`,
+//           file: svgContents,
+//         }).pipe(fs.createWriteStream(pngPath))
+//       } catch (err) { console.log(err) }
+//     })
+//   })
 
-}
+// }
 
 const slugify = s => s.toLowerCase().replace(' ', '-')
 
