45636362b9270b8feabf52403a18cda4310bb9f1
SachaG <358832+SachaG@users.noreply.github.com>
2017-11-09 17:09:41 +0900

    improve nav

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import Link from "gatsby-link"
-
+import Link from 'gatsby-link'
+import classNames from 'classnames'
 import nav from '../../data/nav.json'
 
 const slugify = s => s.toLowerCase().replace(' ', '-')
 
-const NavItem = ({ label, subPages, isActive }) =>
-  <li>
-    <h3 className="nav-page"><Link to={`/2017/${slugify(label)}/`}>{label}</Link></h3>
-    <div className="nav-subpages">
-      {isActive && subPages && subPages.map((page, i) => 
-        <Link key={i} className="nav-subpage" to={`/2017/${slugify(label)}/${slugify(page)}/`}>{page} </Link>
-      )}
-    </div>
-  </li>
+const isActive = (pathname, label) => pathname.indexOf(slugify(label)) !== -1
 
 const Nav = ({ location }) =>
     <div className="nav">
 		<ul>
-			{nav.items.map((item, i) => <NavItem key={i} {...item} isActive={location.pathname.indexOf(slugify(item.label)) !== -1} />)}
+			{nav.items.map((item, i) => <NavItem key={i} {...item} location={location} location={location} />)}
 		</ul>
     </div>
+
+const NavItem = ({ label, subPages, location }) =>
+  <li>
+    <h3 className="nav-page"><Link to={`/2017/${slugify(label)}/`}>{label}</Link></h3>
+    <div className="nav-subpages">
+      {isActive(location.pathname, label) && subPages && subPages.map((subLabel, i) => <NavSubItem key={i} parentLabel={label} label={subLabel} location={location} />)}
+    </div>
+  </li>
+
+const NavSubItem = ({ parentLabel, label, location }) =>
+  <Link className={classNames('nav-subpage', {'nav-subpage-active': isActive(location.pathname, label)})} to={`/2017/${slugify(parentLabel)}/${slugify(label)}/`}>{label} </Link>
 
 export default Nav
