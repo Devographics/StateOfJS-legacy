e2f76d6c385d85cde7ea3d822dc5f57f1dc64446
SachaG <358832+SachaG@users.noreply.github.com>
2017-11-18 18:31:19 +0900

    Add custom node component for bubble chart

--- src/components/charts/OthersBubble.js
+++ src/components/charts/OthersBubble.js
@@ -4,6 +4,38 @@ import { ResponsiveBubble } from 'nivo'
 import { colorRange } from '../../constants'
 import theme from '../../nivoTheme'
 
+const BubbleNode = ({ node, style, handlers }) => {
+    if (style.r <= 0) return null
+
+    const className = node.r < 10 ? 'xsmall' : node.r < 20 ? 'small' : node.r < 50 ? 'medium' : node.r < 100 ? 'large' : 'xlarge';
+
+    const nodeHandlers = node.depth === 0 ? {} : handlers;
+
+    return (
+        <g transform={`translate(${style.x},${style.y})`} className={className}>
+            <circle
+                r={style.r}
+                {...nodeHandlers}
+                fill={style.fill ? style.fill : style.color}
+                stroke={style.borderColor}
+                strokeWidth={style.borderWidth}
+            />
+            {node.label !== false && (
+                <text
+                    textAnchor="middle"
+                    alignmentBaseline="central"
+                    style={{
+                        fill: style.labelTextColor,
+                        pointerEvents: 'none',
+                    }}
+                >
+                    {node.label}
+                </text>
+            )}
+        </g>
+    )
+}
+
 const OthersBubble = ({ data, baseKeys }) => (
     <ResponsiveBubble
         root={{
@@ -25,11 +57,13 @@ const OthersBubble = ({ data, baseKeys }) => (
         colorBy={({ key }) => {
             if (key === 'tools') return '#F4F6F4'
             if (baseKeys.includes(key)) return colorRange[3]
-            return colorRange[0]
+            return colorRange[2]
         }}
+        labelSkipRadius={9}
         motionStiffness={90}
         motionDamping={11}
         theme={theme}
+        nodeComponent={BubbleNode}
     />
 )
 
