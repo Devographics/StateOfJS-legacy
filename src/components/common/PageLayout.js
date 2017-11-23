9f5ac6ee6927c83c25ede7cb21cd73b5965227ba
SachaG <358832+SachaG@users.noreply.github.com>
2017-11-23 18:20:53 +0900

    Add logo

--- src/components/common/PageLayout.js
+++ src/components/common/PageLayout.js
@@ -1,16 +1,26 @@
 import React from 'react'
-
+import Link from 'gatsby-link'
 import Nav from './Nav'
 import PageTitle from './PageTitle'
+import Logo from './Logo'
+
+const Spacer = () => <div className="pagelayout__spacer"/>
 
 const PageLayout = props => (
-    <div className="page-layout">
-        <div className="sidebar">
-            <Nav {...props} />
-        </div>
-        <div className="content">
-            <PageTitle {...props} />
-            {props.children}
+    <div className="pagelayout">
+    	<div className="pagelayout__header">
+    		<Spacer/>
+    		<h1 className="pagelayout__logo"><Link to="/"><Logo width={150}/></Link></h1>
+    		<Spacer/>
+    	</div>
+    	<div className="pagelayout__inner">
+	        <div className="sidebar">
+	            <Nav {...props} />
+	        </div>
+	        <div className="content">
+	            <PageTitle {...props} />
+	            {props.children}
+	        </div>
         </div>
     </div>
 )
