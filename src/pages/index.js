2704ced3b73e579dc71401fd69db3c86e134753b
SachaG <358832+SachaG@users.noreply.github.com>
2018-08-31 10:17:58 +0900

    working on new design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import DocumentTitle from 'react-document-title'
+import React from 'react';
+import Link from 'gatsby-link';
+import DocumentTitle from 'react-document-title';
 
 // import ReactGA from 'react-ga'
 
-import '../stylesheets/screen.scss'
-import Share from '../components/common/Share.js'
-import Results2016 from '../components/common/Results2016.js'
+import '../stylesheets/screen.scss';
+import Share from '../components/common/Share.js';
+import PreviousYears from '../components/common/PreviousYears.js';
 
-import TextBlock from '../components/blocks/TextBlock.js'
+import TextBlock from '../components/blocks/TextBlock.js';
 // import Stats from '../components/common/Stats.js'
-import Comments from '../components/common/Comments.js'
-import About from '../components/common/About.js'
+import Comments from '../components/common/Comments.js';
+import About from '../components/common/About.js';
 
-import Newsletter from '../components/common/Newsletter'
+import Newsletter from '../components/common/Newsletter';
 
 // import FooterContents from '../data/footer.md'
 
@@ -23,75 +23,77 @@ The JavaScript world is richer and messier than ever.
 So we collected data from over 20,000 developers, asking them questions on topics ranging from front-end frameworks and state management, to build tools and testing libraries.
 
 You'll find out which libraries developers want to learn next, and which have the best satisfaction ratings. And hopefully, get a better understanding of the ever-changing JavaScript ecosystem.
-`
+`;
 
 const homeComingSoonContents = `
 We collected data from over 20,000 developers, asking them questions on topics ranging from front-end frameworks to testing libraries and more. 
 
 We're almost ready to release the results to the world. So join us on December 12, 2017 at 5pm PST for a special launch livestream.
-`
+`;
 
 const newsletterContents = `
 Leave us your email and we'll let you know when next year's survey comes out. 
-` 
+`;
 
 export default class Index extends React.Component {
-    render() {
-        return (
-            <DocumentTitle title="The State Of JavaScript 2017">
-                <div className="results-container content home">
-                    <h1 className="logo">
-                        <img src="images/javascript2017.svg" alt="The State Of JavaScript" />
-                        {/*<div className="logo-1"/>
+  render () {
+    return (
+      <DocumentTitle title="The State Of JavaScript 2017">
+        <div className="results-container content home">
+          <h1 className="logo section-narrow">
+            <img src="images/stateofjs2018.svg" alt="The State Of JavaScript" />
+            {/*<div className="logo-1"/>
             <div className="logo-2"/>*/}
-                    </h1>
+          </h1>
 
-                    <div className="bubbles">
-                        <h3 className="bubble bubble-great">
-                            <img src="images/left-speech-bubble.svg" />
-                            <span>JavaScript is great!</span>
-                        </h3>
-                        <h3 className="bubble bubble-mess">
-                            <img src="images/right-speech-bubble.svg" />
-                            <span>JavaScript is a mess!</span>
-                        </h3>
-                    </div>
+          {/* <div className="bubbles">
+            <h3 className="bubble bubble-great">
+              <img alt="JavaScript is great!" src="images/left-speech-bubble.svg" />
+              <span>JavaScript is great!</span>
+            </h3>
+            <h3 className="bubble bubble-mess">
+              <img alt="JavaScript is a mess!" src="images/right-speech-bubble.svg" />
+              <span>JavaScript is a mess!</span>
+            </h3>
+          </div> */}
 
-                    <div className="intro section-border section-narrow">
-                        <TextBlock text={homeContents} />
-                        <div className="view-results">
-                            <Link className="button large-button" to="/2017/introduction/">
-                                View Results
-                            </Link>
-                        </div>
-                    </div>
-                    {/*<Stats />*/}
+          <div className="intro section-narrow">
+            <TextBlock text={homeContents} />
+            <div className="take-survey">
+              <Link className="button large-button" to="/2017/introduction/">
+                Take Survey
+              </Link>
+            </div>
+          </div>
+          {/*<Stats />*/}
 
-                    <Results2016 />
+          <PreviousYears />
 
-                    <Share />
+          <Share />
 
-                    <Comments />
+          {/* <Comments /> */}
 
-                    <About />
+          <About />
 
-                    <div className="block block--newsletter--home">
-                        <TextBlock title="Stay Tuned" text={newsletterContents} />
-                        <Newsletter />
-                    </div>
+          <div className="block block--newsletter--home">
+            <TextBlock title="Stay Tuned" text={newsletterContents} />
+            <Newsletter />
+          </div>
 
-                    <div className="footer home-footer">
-                        <TextBlock>
-                            <p>
-                                Hand-coded in Osaka, Japan with <a href="http://nivo.rocks">Nivo</a> & <a href="http://gatsbyjs.org">Gatsby</a>. Extra data from <a href="https://bestof.js.org/">BestOfJS</a>.&nbsp;
-                                <a href="https://github.com/StateOfJS/StateOfJS">Leave an issue</a>
-                                &nbsp;or&nbsp;
-                                <a href="mailto:hello@stateofjs.com">Get in touch</a>.
-                            </p>
-                        </TextBlock>
-                    </div>
-                </div>
-            </DocumentTitle>
-        )
-    }
+          <div className="footer home-footer">
+            <TextBlock>
+              <p>
+                Hand-coded in Osaka, Japan with <a href="http://nivo.rocks">Nivo</a> &{' '}
+                <a href="http://gatsbyjs.org">Gatsby</a>. Extra data from <a href="https://bestof.js.org/">BestOfJS</a>
+                .&nbsp;
+                <a href="https://github.com/StateOfJS/StateOfJS">Leave an issue</a>
+                &nbsp;or&nbsp;
+                <a href="mailto:hello@stateofjs.com">Get in touch</a>.
+              </p>
+            </TextBlock>
+          </div>
+        </div>
+      </DocumentTitle>
+    );
+  }
 }
