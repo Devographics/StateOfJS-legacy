f36ee16983fad35fc1832c1cc473c706ab629f1a
michaelrambeau <mikeair@gmail.com>
2018-09-03 06:47:39 +0900

    Use `...` operator instead of Object.assign. TODO: fix ESLint warning

--- src/components/common/PeriodicTable/PeriodicTableContainer.js
+++ src/components/common/PeriodicTable/PeriodicTableContainer.js
@@ -7,30 +7,26 @@ import PeriodicTable from './PeriodicTable'
 
 const repeatArray = (array, number) => times(number).reduce(acc => acc.concat(array), [])
 
-class PeriodicTableContainer extends Component {
-  render () {
-    const { data } = this.props
-    const allProjects = data.allProject.edges.map(edge => edge.node)
-    const findProject = id => allProjects.find(project => project.id === id)
-    console.info(settings)
-    const categories = Object.values(settings)
-    const addData = project => {
-      const foundProject = findProject(project.id)
-      return Object.assign({}, foundProject, project) // the order matters, `project` values (from yaml) override `foundProject` values
-    }
-
-    const elements = flatten(
-      categories.map(({ color, projects }) =>
-        projects.map(project => Object.assign({}, addData(project), { color }))
-      )
-    )
-    const repeatedElements = repeatArray(elements, 4)
-    return <PeriodicTable elements={repeatedElements} />
+const PeriodicTableContainer = ({ data }) => {
+  const allProjects = data.allProject.edges.map(edge => edge.node)
+  const findProject = id => allProjects.find(project => project.id === id)
+  const categories = Object.values(settings)
+  const addBestOfJavaScriptData = project => {
+    const foundProject = findProject(project.id)
+    return { ...foundProject, ...project } // the order matters, `project` values (from yaml) override `foundProject` values
   }
+  const elements = flatten(
+    categories.map(({ color, projects }) =>
+      projects.map(project => ({ ...addBestOfJavaScriptData(project), color }))
+    )
+  )
+  // Let's duplicate N times data to fill the screen
+  const repeatedElements = repeatArray(elements, 4)
+  return <PeriodicTable elements={repeatedElements} />
 }
 
 PeriodicTableContainer.propTypes = {
-  data: PropTypes.object.isRequired, // provided by graphQL
+  data: PropTypes.object.isRequired, // provided by GraphQL data store (see `layouts/index.js`)
 }
 
 export default PeriodicTableContainer
