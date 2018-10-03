87463edbf73b4ca8dfc8817dd6f824de37b7dc42
SachaG <358832+SachaG@users.noreply.github.com>
2018-10-03 09:19:51 +0900

    Fix periodic table and homepage

--- home/gatsby-node.js
+++ home/gatsby-node.js
@@ -1,3 +1,7 @@
+const bestofjsNameToSlug = require('./src/helpers/bestofjs-slugify')
+const got = require('got')
+const crypto = require('crypto')
+
 /**
  * Fix case for pages path, it's not obvious on OSX which is case insensitive,
  * but on some environments (eg. travis), it's a problem.
@@ -23,4 +27,36 @@ exports.onCreatePage = ({ page, boundActionCreators }) => {
 
         resolve()
     })
+}
+
+
+/*
+Add `Project` nodes to the application GraphQL data store
+See `allProject` query on http://localhost:8000/___graphql
+We fetch about 500 projects from Best of JavaScript
+*/
+exports.sourceNodes = async ({ boundActionCreators }) => {
+    const { createNode } = boundActionCreators
+    const url = 'https://bestofjs-api-v2.firebaseapp.com/projects.json'
+    const data = await got(url, { json: true }).then(r => r.body)
+    const { projects } = data
+    const sortedProjects = projects
+        .filter(project => project.stars >= 5000)
+        .sort((a, b) => (a.stars > b.stars ? -1 : 1))
+    const nodes = sortedProjects.map(project => ({
+        id: bestofjsNameToSlug(project.name),
+        name: project.name,
+        stars: project.stars,
+        parent: null,
+        children: [],
+        internal: {
+            type: `Project`,
+            contentDigest: crypto
+                .createHash(`md5`)
+                .update(JSON.stringify(project))
+                .digest(`hex`),
+        },
+        name: project.name,
+    }))
+    nodes.forEach(node => createNode(node))
 }
---
+++ home/src/components/HomeLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import PeriodicTable from './PeriodicTable'
+
+const HomeLayout = props => (
+  <div>
+    <PeriodicTable {...props} />
+    <div className="home-layout-wrapper">
+      <h1 className="logo">
+        <img src="images/stateofjs2018-logo.svg" alt="The State Of JavaScript" />
+        {/* <div className="logo-1"/>
+            <div className="logo-2"/>*/}
+      </h1>
+      <div className="home-layout">{props.children}</div>
+    </div>
+  </div>
+)
+
+export default HomeLayout
