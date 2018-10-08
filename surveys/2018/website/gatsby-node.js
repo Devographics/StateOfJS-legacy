72d199cf2a5b51bbb9adb8f26975b1bb0a40917c
SachaG <358832+SachaG@users.noreply.github.com>
2018-10-08 10:24:44 +0900

    Migrate to v2 syntax

--- surveys/2018/website/gatsby-node.js
+++ surveys/2018/website/gatsby-node.js
@@ -3,22 +3,30 @@ const fs = require('fs')
 
 const nav = yaml.safeLoad(fs.readFileSync('./src/data/nav.yaml', 'utf8'))
 
-exports.modifyWebpackConfig = function({ config }) {
-    config.removeLoader('md')
-    config.loader('md', cfg => {
-        cfg.test = /\.md$/
-        cfg.loader = 'html!markdown'
+/*
 
-        return cfg
-    })
+TODO: use https://www.gatsbyjs.org/docs/actions/#setWebpackConfig
+see: https://www.gatsbyjs.org/docs/migrating-from-v1-to-v2/#change-modifywebpackconfig-to-oncreatewebpackconfig
 
-    return config
-}
+(not sure if we still need this though…)
+*/
+
+// exports.modifyWebpackConfig = function({ config }) {
+//     config.removeLoader('md')
+//     config.loader('md', cfg => {
+//         cfg.test = /\.md$/
+//         cfg.loader = 'html!markdown'
+
+//         return cfg
+//     })
+
+//     return config
+// }
 
 const slugify = s => s.toLowerCase().replace(' ', '-')
 
-exports.createPages = async ({ boundActionCreators }) => {
-    const { createRedirect } = boundActionCreators
+exports.createPages = async ({ actions }) => {
+    const { createRedirect } = actions
 
     nav.forEach(item => {
         if (item.subPages) {
@@ -37,7 +45,7 @@ exports.createPages = async ({ boundActionCreators }) => {
 }
 
 /**
- * Fix case for pages path, it's not obvious on OSX which is cas insensitive,
+ * Fix case for pages path, it's not obvious on OSX which is case insensitive,
  * but on some environments (eg. travis), it's a problem.
  *
  * Many pages are created from components, and we use upper first in that case
@@ -46,8 +54,8 @@ exports.createPages = async ({ boundActionCreators }) => {
  * Implement the Gatsby API “onCreatePage”.
  * This is called after every page is created.
  */
-exports.onCreatePage = ({ page, boundActionCreators }) => {
-    const { createPage, deletePage } = boundActionCreators
+exports.onCreatePage = ({ page, actions }) => {
+    const { createPage, deletePage } = actions
 
     return new Promise(resolve => {
         const newPage = Object.assign({}, page, {
