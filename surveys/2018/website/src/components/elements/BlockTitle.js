d876fdf24c905f8519a69837b29580c973b1e7ef
Raphaël Benitte <raphael.benitte@ekino.com>
2018-10-16 12:07:24 +0900

    use proper tool name instead of id for titles

--- surveys/2018/website/src/components/elements/BlockTitle.js
+++ surveys/2018/website/src/components/elements/BlockTitle.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { getWording } from '../../helpers/wording'
+import { getWording, getToolName } from '../../helpers/wording'
 import ShareChart from '../common/ShareChart'
 
 const BlockTitle = ({ chart, tool }) => (
     <div className="block__title">
-        <h3 className="block__title__text">{getWording(`charts.${chart}`, { tool })}</h3>
+        <h3 className="block__title__text">
+            {getWording(`charts.${chart}`, { tool: getToolName(tool) })}
+        </h3>
         <ShareChart className="block__title__share" chart={chart} />
     </div>
 )
