370472a8553cf4bffa06e3035b20c31aeffaa331
Raphaël Benitte <raphael.benitte@ekino.com>
2018-10-16 12:29:14 +0900

    enable header block for tools which are not part of bestofjs

--- surveys/2018/website/src/components/blocks/ToolHeaderBlock.js
+++ surveys/2018/website/src/components/blocks/ToolHeaderBlock.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { format } from 'd3-format'
+import { getToolName } from '../../helpers/wording'
 import bestOfJsData from '../../data/bestofjs'
 import PeriodicTableElementHtml from '../elements/PeriodicTableElementHtml'
 
@@ -17,16 +18,6 @@ export default class ToolHeaderBlock extends Component {
 
         const project = bestOfJsData.projects.find(p => p.slug === tool)
 
-        if (project === undefined) {
-            return (
-                <div style={{ color: 'red' }}>
-                    No project found in best of js data matching tool: <strong>{tool}</strong>
-                    <br />
-                    <br />
-                </div>
-            )
-        }
-
         return (
             <div className="ToolHeader">
                 <div>
@@ -34,23 +25,31 @@ export default class ToolHeaderBlock extends Component {
                 </div>
                 <div>
                     <div className="ToolHeader_Header">
-                        <h2 className="ToolHeader_Title">{project.name}</h2>
-                        <div>{starsFormatter(project.stars)} stars</div>
+                        <h2 className="ToolHeader_Title">{getToolName(tool)}</h2>
+                        {project && <div>{starsFormatter(project.stars)} stars</div>}
                     </div>
-                    <div>{project.description}</div>
-                    <div className="ToolHeader_Links">
-                        <a href={project.homepage} target="_blank" rel="noopener noreferrer">
-                            {project.homepage}
-                        </a>
-                        {' | '}
-                        <a
-                            href={`https://github.com/${project.github}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            github
-                        </a>
-                    </div>
+                    {project && (
+                        <Fragment>
+                            <div>{project.description}</div>
+                            <div className="ToolHeader_Links">
+                                <a
+                                    href={project.homepage}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    {project.homepage}
+                                </a>
+                                {' | '}
+                                <a
+                                    href={`https://github.com/${project.github}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    github
+                                </a>
+                            </div>
+                        </Fragment>
+                    )}
                 </div>
             </div>
         )
