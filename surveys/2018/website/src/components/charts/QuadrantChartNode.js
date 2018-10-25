ffce65e40187c185b59ef74ffaabdddd26be7ef2
Raphaël Benitte <raphael.benitte@ekino.com>
2018-10-25 22:12:56 +0900

    inject data for other tools

--- surveys/2018/website/src/components/charts/QuadrantChartNode.js
+++ surveys/2018/website/src/components/charts/QuadrantChartNode.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { getToolName } from '../../helpers/wording'
 import PeriodicElement from '../elements/PeriodicElement'
