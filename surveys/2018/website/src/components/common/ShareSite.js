c4cbd21950b50e2177e32cda040c6539ac31e73f
SachaG <358832+SachaG@users.noreply.github.com>
2018-11-19 12:46:52 +0900

    Add linkedin to sidebar

--- surveys/2018/website/src/components/common/ShareSite.js
+++ surveys/2018/website/src/components/common/ShareSite.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Twitter, Email, Facebook } from './ShareChart'
+import { Twitter, Email, Facebook, LinkedIn } from './ShareChart'
 
 const link = 'https://2018.stateofjs.com'
-const twitterText = `Discover the #StateOfJS 2018 results ${link}`
+const title = 'Discover the StateOf JavaScript 2018 results'
+const twitterText = `Discover the State Of JavaScript 2018 results ${link} #StateOfJS`
 const subject = 'State Of JavaScript Survey Results'
 const body = `Here are some interesting JavaScript survey results: ${link}`
 
@@ -10,6 +11,7 @@ const ShareSite = () => (
     <div className="ShareSite">
         <Twitter text={twitterText} />
         <Facebook link={link} />
+        <LinkedIn link={link} title={title}/>
         <Email subject={subject} body={body} />
     </div>
 )
