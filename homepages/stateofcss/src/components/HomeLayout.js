715ce9cdba3d3cb77c5a074c2c7452a97097b7e2
SachaG <358832+SachaG@users.noreply.github.com>
2019-01-16 17:33:31 +0900

    work on animation

--- homepages/stateofcss/src/components/HomeLayout.js
+++ homepages/stateofcss/src/components/HomeLayout.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Illustration from './Illustration'
 
 const HomeLayout = props => (
     <div>
         <div className="home-layout">
             <div className="home-logo">
                 <h1 className="logo">
-                    <img src="images/stateofcss-illustration.svg" alt="The State Of CSS" />
+                    <Illustration/>
                 </h1>
-                <a className="home-take-survey" href="#">Take the Survey</a>
+                {/* <a className="home-take-survey" href="#">Take the Survey</a> */}
             </div>
             <div className="home-content">{props.children}</div>
         </div>
